976ec796514ee63831b9c378deb5df135518bc9a
yashk21 <yashk8119@gmail.com>
2024-03-02 23:16:27 +0530

    update schema for lvls updated

--- server/Schema/Lvls.js
+++ server/Schema/Lvls.js
@@ -1,1 +1,11 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+
+const lvlSchema = new mongoose.Schema({
+  img: String,
+});
+const lvl = new mongoose.model("Lvl", lvlSchema);
+module.exports = lvl
+// // const lvl1 = new lvl({
+// //   img: "ckcbdsch2nf33f3",
+// // });
+// // lvl1.save();
